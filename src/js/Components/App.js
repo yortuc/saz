24d6aedc269ad67cff3690adc1c833cb54d7edd5
Evren Yortuçboylu <evox@Evrens-MacBook-Pro.local>
2015-10-31 21:11:24 +0200

    initial commit

---
+++ src/js/Components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';  
+
+class App extends Component {  
+    constructor() {
+        super();
+        this.state = {
+            // default state
+        }
+    }
+
+    render() { 
+    	return (
+    		<div>hello es6</div>
+    	)
+    }
+}
+
+export default App;
---
+++ src/main.js
@@ -0,0 +1,4 @@
+ import React from 'react';  
+ import App from './js/Components/App';  
+
+ React.render(<App />, document.getElementById('content'));
